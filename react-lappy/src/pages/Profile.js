7697a64862ba8488b99c7c531fca8a0bdb9b814a
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-12-15 16:13:07 +0100

    Attempting to change profile pic

--- react-lappy/src/pages/Profile.js
+++ react-lappy/src/pages/Profile.js
@@ -15,14 +15,32 @@ const Profile = ({loggedid}) => {
 
     const url = "http://127.0.0.1:8000/api/users/"
 
-    if (id != null && user.username == "default") {fetch(url.concat(id)).then((res) => res.json()).then((data) => {setUser(data); console.log(data)})}
+    if (id != null && user.username == "default") {fetch(url.concat(id)).then((res) => res.json()).then((data) => {setUser(data); 
+   //     console.log(data)
+    })}
 
+    const avatarurl = "http://127.0.0.1:8000/api/profiles/"
     
+    var profilepic = profilepictest;
+    console.log(profilepic)
+    async function getProfilePic(){
+        setTimeout(async() => {
+            const response = await fetch(avatarurl.concat(user.userprofile))
+            let profile = (await response.json()).data.avatar;
+            profilepic = profile
+        }, 3000);}
+    
+    getProfilePic()
+        // console.log(profilepic)
+        // return fetch(avatarurl.concat(user.userprofile)).then((res) => res.json()).then((data) => {profilepic = data.avatar; console.log(data.avatar)})
+
+    // await getProfilePic()
+
 
     return (
         <div className="profile-content">
             <div className="profilepic">
-                <img src={profilepictest} alt="Profile picture" />
+                <img src= {profilepic.result} alt="Profile picture" />
             </div>
             <div className="user-info">
                 <p>Username: {user.username}</p>
