7457f53df80c9709cab0ede9b1a07eb5a0c253b9
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-12-15 16:32:01 +0100

    Logged in users cant see the login page

--- react-lappy/src/components/Navbar.js
+++ react-lappy/src/components/Navbar.js
@@ -4,14 +4,23 @@ import Profile from "../pages/Profile"
 import Searchbar from "./Searchbar"
 
 const Navbar = ({id}) => {
+
+    var option1;
+
+    var option2;
+
+    if (id != null){ //logged in
+    option1 = <Link to= {`/profile/${id}`}>Profile</Link>
+    }
+    else option1 = <Link to="/login">Login</Link>
+    
     return (
         <nav className="navigation">
             <h1>Lappy.gp</h1>
             <div className="links">
                 <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
+                {option1}
                 <Link to="/register">Register</Link>
-                <Link to= {`/profile/${id}`}>Profile</Link>
                 <Link to="/circuitMap">Circuit Map</Link>
             </div>
             <div className="search">
