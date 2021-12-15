f55f613a69a8fa84c68de3c4892898de44104c2a
Lennert Saerens <lennert.saerens@vub.be>
2021-12-15 19:57:19 +0100

    created dropdown for circuits in add laptime

--- react-lappy/src/pages/AddLaptime.jsx
+++ react-lappy/src/pages/AddLaptime.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
 const AddLaptime = () => {
+
+    const [circuits, setCircuits] = useState([
+        {
+            id: 1,
+            name: "Spa-Francoshamps",
+            coordinates: { lat: 50.436430, lng: 5.970263 }
+        }, {
+            id: 2,
+            name: "Zolder",
+            coordinates: { lat: 50.990359, lng: 5.257508 }
+        }
+    ])
+
     return (
         <div className="add-laptime">
             <h1>Add Laptime</h1>
             <form action="#" method="post">
                 <p>
                     <label for="circuit">Circuit:</label>
-                    <input type="text" id="circuit" name="circuit" required></input>
+                    <select name="circuit" id="circuit">
+                        {circuits.map((circuit) => (
+                            <option value={circuit.name}>{circuit.name}</option>
+                        ))}
+                    </select>
                 </p>
                 <p>
                     <label for="laptime">Laptime:</label>
