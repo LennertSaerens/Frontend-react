610dfd1d4681322f3a89c12c5c64b08b85799b84
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2021-12-15 16:22:33 +0100

    Implemented avatar from database

--- react-lappy/src/pages/Profile.js
+++ react-lappy/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import profilepictest from "../images/profilepictest.png"
 import { useParams } from "react-router-dom";
 
@@ -6,10 +6,11 @@ const Profile = ({loggedid}) => {
 
     const visitid = useParams()
 
+    const[profilePic, setProfilePic] = useState(profilepictest)
+
     var id = visitid.id
 
     const [user, setUser] = useState(
-        
          { id: 1, username: "default", first_name: "Lennert", last_name: "Saerens", date_joined: "24/09/2021", numfollowers: 69 }
     )
 
@@ -21,26 +22,15 @@ const Profile = ({loggedid}) => {
 
     const avatarurl = "http://127.0.0.1:8000/api/profiles/"
     
-    var profilepic = profilepictest;
-    console.log(profilepic)
-    async function getProfilePic(){
-        setTimeout(async() => {
-            const response = await fetch(avatarurl.concat(user.userprofile))
-            let profile = (await response.json()).data.avatar;
-            profilepic = profile
-        }, 3000);}
-    
-    getProfilePic()
-        // console.log(profilepic)
-        // return fetch(avatarurl.concat(user.userprofile)).then((res) => res.json()).then((data) => {profilepic = data.avatar; console.log(data.avatar)})
-
-    // await getProfilePic()
+    console.log(profilePic)
+    fetch(avatarurl.concat(user.userprofile)).then(res => res.json()).then(data => setProfilePic(data.avatar))
+    console.log(profilePic)
 
 
     return (
         <div className="profile-content">
             <div className="profilepic">
-                <img src= {profilepic.result} alt="Profile picture" />
+                <img src= {profilePic} alt="Profile picture" />
             </div>
             <div className="user-info">
                 <p>Username: {user.username}</p>
