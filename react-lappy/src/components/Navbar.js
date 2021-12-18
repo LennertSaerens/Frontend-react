34d600da898e32677fcc46ab509044ef12da9aa7
Lennert Saerens <lennert.saerens@vub.be>
2021-12-18 11:04:24 +0100

    Implements conditional rendering of navbar

--- react-lappy/src/components/Navbar.js
+++ react-lappy/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Searchbar from "./Searchbar"
 
@@ -7,15 +7,20 @@ import Searchbar from "./Searchbar"
  * @returns Code for the navbar at the top of the page. 
  */
 const Navbar = () => {
+
+    const [loggedIn, setLoggedIn] = useState(true)
+
     return (
         <nav className="navigation">
             <h1>Lappy.gp</h1>
             <div className="links">
                 <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/profile">Profile</Link>
                 <Link to="/circuitMap">Circuit Map</Link>
+                {loggedIn
+                    ? <Link to="/profile">Profile</Link>
+                    : <Link to="/login">Login</Link>
+                }
+                <Link to={loggedIn ? "/logout" : "/register"}>{loggedIn ? "Logout" : "Register"}</Link>
             </div>
             <div className="search">
                 <Searchbar />
